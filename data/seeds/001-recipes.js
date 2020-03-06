c69513683984f098fd68566aa020e8399745ff10
Sheila R Moore <sherexmykes@github.com>
2020-03-05 21:36:58 -0500

    migrations and seeds

---
+++ data/seeds/001-recipes.js
@@ -0,0 +1,15 @@
+
+exports.seed = function(knex) {
+  // Deletes ALL existing entries
+  return knex('recipes').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('recipes').insert([
+        {name: 'eggs'},
+        {name: 'bread'},
+        {name: 'sandwich'}
+      ]);
+  
+    
+  });
+};
